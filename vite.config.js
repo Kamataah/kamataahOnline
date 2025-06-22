ca3ba15c6c422afc88711d3463db8f6a73d8d53b
Chetan Tidgam <cgtidgam@bajajext.co.in>
2025-06-22 21:17:09 +0530

    chnages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 // import eslint from 'vite-plugin-eslint'
 export default defineConfig({
-  base: '/',
+  base: '/kamataahOnline/',
   plugins: [
     react({
       babel: {
