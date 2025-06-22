9e110debc4bbead6e019f8126108c3e9721594ab
Chetan Tidgam <cgtidgam@bajajext.co.in>
2025-06-22 23:44:12 +0530

    changes

--- src/Components/user/HomePageContent.jsx
+++ src/Components/user/HomePageContent.jsx
@@ -47,52 +47,91 @@ function HomePageContent() {
                     </div>
                 </div>
             </section>
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-xl-3 col-lg-4 col-md-6 col-sm-12 col-xs-12">
-                        <div className="single_arrivel_item weidth_1 mix shoes">
-                            <img src="/assets/img/logo.png" alt="#" />
-                            <div className="hover_text">
-                                <p>Canvas</p>
-                                <a href="single-product.html"><h3>Lorem Canvas Low-Top Sneaker</h3></a>
-                                <div className="rate_icon">
-                                    <a href="#"> <i className="fas fa-star"></i> </a>
-                                    <a href="#"> <i className="fas fa-star"></i> </a>
-                                    <a href="#"> <i className="fas fa-star"></i> </a>
-                                    <a href="#"> <i className="fas fa-star"></i> </a>
-                                    <a href="#"> <i className="fas fa-star"></i> </a>
+            <section className="new_arrival section_padding">
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-lg-12">
+                            <div className="new_arrival_iner filter-container">
+                                <div className="single_arrivel_item weidth_1 mix shoes">
+                                    <img src="/assets/img/logo.png" alt="#" />
+                                    <div className="hover_text">
+                                        <p>Canvas</p>
+                                        <h5>Lorem Canvas Low-Top Sneaker</h5>
+                                        <div className="rate_icon">
+                                            <a href="#"> <i className="fas fa-star"></i> </a>
+                                            <a href="#"> <i className="fas fa-star"></i> </a>
+                                            <a href="#"> <i className="fas fa-star"></i> </a>
+                                            <a href="#"> <i className="fas fa-star"></i> </a>
+                                            <a href="#"> <i className="fas fa-star"></i> </a>
+                                        </div>
+                                        <h6>$150</h6>
+                                        <div className="social_icon">
+                                            <a href="#"><i className="ti-heart"></i></a>
+                                            <a href="#"><i className="ti-bag"></i></a>
+                                        </div>
+                                    </div>
                                 </div>
-                                <h5>$150</h5>
-                                <div className="social_icon">
-                                    <a href="#"><i className="ti-heart"></i></a>
-                                    <a href="#"><i className="ti-bag"></i></a>
+                                <div className="single_arrivel_item weidth_1 mix shoes">
+                                    <img src="/assets/img/logo.png" alt="#" />
+                                    <div className="hover_text">
+                                        <p>Canvas</p>
+                                        <h5>Lorem Canvas Low-Top Sneaker</h5>
+                                        <div className="rate_icon">
+                                            <a href="#"> <i className="fas fa-star"></i> </a>
+                                            <a href="#"> <i className="fas fa-star"></i> </a>
+                                            <a href="#"> <i className="fas fa-star"></i> </a>
+                                            <a href="#"> <i className="fas fa-star"></i> </a>
+                                            <a href="#"> <i className="fas fa-star"></i> </a>
+                                        </div>
+                                        <h6>$150</h6>
+                                        <div className="social_icon">
+                                            <a href="#"><i className="ti-heart"></i></a>
+                                            <a href="#"><i className="ti-bag"></i></a>
+                                        </div>
+                                    </div>
+                                </div>\<div className="single_arrivel_item weidth_1 mix shoes">
+                                    <img src="/assets/img/logo.png" alt="#" />
+                                    <div className="hover_text">
+                                        <p>Canvas</p>
+                                        <h5>Lorem Canvas Low-Top Sneaker</h5>
+                                        <div className="rate_icon">
+                                            <a href="#"> <i className="fas fa-star"></i> </a>
+                                            <a href="#"> <i className="fas fa-star"></i> </a>
+                                            <a href="#"> <i className="fas fa-star"></i> </a>
+                                            <a href="#"> <i className="fas fa-star"></i> </a>
+                                            <a href="#"> <i className="fas fa-star"></i> </a>
+                                        </div>
+                                        <h6>$150</h6>
+                                        <div className="social_icon">
+                                            <a href="#"><i className="ti-heart"></i></a>
+                                            <a href="#"><i className="ti-bag"></i></a>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-xl-3 col-lg-4 col-md-6 col-sm-12 col-xs-12">
-                        <div className="single_arrivel_item weidth_1 mix shoes">
-                            <img src="/assets/img/logo.png" alt="#" />
-                            <div className="hover_text">
-                                <p>Canvas</p>
-                                <a href="single-product.html"><h3>Lorem Canvas Low-Top Sneaker</h3></a>
-                                <div className="rate_icon">
-                                    <a href="#"> <i className="fas fa-star"></i> </a>
-                                    <a href="#"> <i className="fas fa-star"></i> </a>
-                                    <a href="#"> <i className="fas fa-star"></i> </a>
-                                    <a href="#"> <i className="fas fa-star"></i> </a>
-                                    <a href="#"> <i className="fas fa-star"></i> </a>
-                                </div>
-                                <h5>$150</h5>
-                                <div className="social_icon">
-                                    <a href="#"><i className="ti-heart"></i></a>
-                                    <a href="#"><i className="ti-bag"></i></a>
+                                <div className="single_arrivel_item weidth_1 mix shoes">
+                                    <img src="/assets/img/logo.png" alt="#" />
+                                    <div className="hover_text">
+                                        <p>Canvas</p>
+                                        <h5>Lorem Canvas Low-Top Sneaker</h5>
+                                        <div className="rate_icon">
+                                            <a href="#"> <i className="fas fa-star"></i> </a>
+                                            <a href="#"> <i className="fas fa-star"></i> </a>
+                                            <a href="#"> <i className="fas fa-star"></i> </a>
+                                            <a href="#"> <i className="fas fa-star"></i> </a>
+                                            <a href="#"> <i className="fas fa-star"></i> </a>
+                                        </div>
+                                        <h6>$150</h6>
+                                        <div className="social_icon">
+                                            <a href="#"><i className="ti-heart"></i></a>
+                                            <a href="#"><i className="ti-bag"></i></a>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </section>
         </div>
 
     );
