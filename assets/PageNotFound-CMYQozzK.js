50eeac2f7da21de9d08b967d813ec1f7b28f99ca
Kamataah <Kamataah@users.noreply.github.com>
2025-06-24 20:22:38 +0000

    deploy: 2a829931dce3e24a77d0504892cebbeddf14e867

--- assets/PageNotFound-CMYQozzK.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-C4P09KZT.js";function t(){return e.jsxs("div",{style:{marginTop:"5%",textAlign:"center"},children:[e.jsx("h1",{children:"404 - Page Not Found"}),e.jsx("p",{children:"Sorry, the page you are looking for does not exist."})]})}export{t as default};
